cf2788396e8e9c8b6bb3c04e4b47d9a8d590bdea
felipeberger <feconchaber@gmail.com>
2021-07-12 17:34:02 +1100

    Implementation of properties page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     render 'home'
   end
+
+  def property
+    @data = { property_id: params[:id] }.to_json
+    render 'property'
+  end
+
 end
