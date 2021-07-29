4c78f621c09f666ce1c2ac712c833c7f8b00fd0f
felipeberger <feconchaber@gmail.com>
2021-07-29 17:04:26 +1100

    layout now includes a responsive dropdown menu, which shows different information depending on the user being authenticated or not

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,18 +1,63 @@
 // layout.js
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import LoginWidget from './login/loginWidget';
+
+
+const loggedIn = () => {
+  return (
+    <>
+    <a className="dropdown-item" href="">Messages</a>
+    <a className="dropdown-item" href="">Notifications</a>
+    <a className="dropdown-item" href="">Trips</a>
+    <div className="dropdown-divider"></div>
+    <a className="dropdown-item" href="">Manage Listings</a>
+    <a className="dropdown-item" href="">Host an Experience</a>
+    <div className="dropdown-divider"></div>
+    <a className="dropdown-item" href="">Help</a>
+    <a className="dropdown-item" href="">Log Out</a>
+    </>
+  )
+}
+
+const notLoggedIn = () => {
+  return (
+    <>
+    <a className="dropdown-item" id="log-in" href="/login" >Log In</a>
+    <a className="dropdown-item" id="sign-up" href="">Sign Up</a>
+    <div className="dropdown-divider"></div>
+    <a className="dropdown-item" href="">Host Your Home</a>
+    <a className="dropdown-item" href="">Host an Experience</a>
+    <a className="dropdown-item" href="">Help</a>
+    </>
+  )
+}
 
 const Layout = (props) => {
+  const [dropDownData, setDropdown] = useState(notLoggedIn)
+
+  useEffect( ()=>{
+    if (props.isLoggedIn) {
+      setDropdown(loggedIn);
+    } else {
+      setDropdown(notLoggedIn);
+    }
+  })
+
   return (
     <React.Fragment>
-      <nav className="navbar navbar-expand navbar-light bg-light">
+      <nav className="navbar navbar-light bg-light">
         <a href="/"><span className="navbar-brand mb-0 h1 text-danger">Airbnb</span></a>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-          </ul>
+
+        <div className="dropleft">
+          <button className="btn rounded dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="dropdown-menu rounded mr-2">
+            {dropDownData}
+          </div>
         </div>
+
       </nav>
       {props.children}
       <footer className="p-3 bg-light">
