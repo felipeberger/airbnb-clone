b87bae8b031d47b73b76609071af857e1a5f67d2
felipeberger <feconchaber@gmail.com>
2021-08-25 17:31:50 +1100

    Partial implementation of the hosting page and listing component. The page now successfully displays owned properties and bookings for each property. Next step is to create a listings page where the user can update the listing details

--- app/javascript/src/hosting/hosting.jsx
+++ app/javascript/src/hosting/hosting.jsx
@@ -6,7 +6,6 @@ import Listing from "./listing";
 
 export default function Hosting () {
     const [listings, setListings] = useState(null)
-    const [bookings, setBookings] = useState(null)
     const [authenticated, setAuthenticated] = useState(false)
     const [username, setUsername] = useState(null)
 
@@ -27,91 +26,17 @@ export default function Hosting () {
         })
     }, [username])
 
-    const getBookings = () => {
-        for(let i = 0; i < listings.length; i++) {
-            fetch(`api/properties/${listings[i].id}/bookings`)
-                .then(handleErrors)
-                .then(data => {
-                    setBookings(data.bookings)
-                    console.log("bookings", data.bookings)
-                    console.log("listings", listings)
-                })
-        }
-    }
-
-    const displayListings = (bool) => {
-        if (!bool) {
-            return (
-            <>
-            <div className="row my-3 pt-2">
-                <div className="col-4 listing-preview pr-sm-1 pr-md-0">
-                    <div key={0} className="card" style={{width: "14rem"}}>
-                        <img className="card-img-top" src="https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=6&m=1040250650&s=612x612&w=0&h=Ve0znmMwCbVyo66uIfeSrSYRuHau85oBiVIv1OplATs=" alt="No properties found" />
-                        <div className="card-body text-center">
-                            <button className="card-title btn btn-secondary btn-lg btn-block" onClick={getBookings}>Upcoming Bookings</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-5 my-3">
-                    <h4>Property Title</h4>
-                    <p>description</p>
-                    <p>City</p>
-                    <p>Country</p>
-                    <p>Price per night</p>
-                </div>
-                <div className="col-3 d-flex flex-column my-3">
-                    <button className="btn btn-danger my-2">Edit listing</button>
-                    <button className="btn btn-danger my-2">Disable listing</button>
-                    <button className="btn btn-danger my-2">Delete listing</button>
-                </div>
-                <div className="upcoming-bookings pt-3 pb-2 d-none">
-                    <h5 className="pb-2">Upcoming bookings:</h5>
-                    <div className="row">
-                        <div className="col-4">
-                            <div className="card" style={{width: "11rem"}}>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">Guest: xx</li>
-                                    <li className="list-group-item">Start: xxx</li>
-                                    <li className="list-group-item">End: xxx</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-4">
-                            <div className="card" style={{width: "11rem"}}>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">Guest: xx</li>
-                                    <li className="list-group-item">Start: xxx</li>
-                                    <li className="list-group-item">End: xxx</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-4">
-                            <div className="card" style={{width: "11rem"}}>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">Guest: xx</li>
-                                    <li className="list-group-item">Start: xxx</li>
-                                    <li className="list-group-item">End: xxx</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr />
-            </>
-            )
-        } 
- 
-    }
-
-
     return (
         <Layout isLoggedIn={authenticated}>
             <div className="container">
-                {displayListings(false)}
-                {listings? <Listing property={listings[2]} />:null}
-                
-                
+                {/* {listings? <Listing property={listings[2]} />:null} */}
+                {listings? listings.map( property => {
+                    return (
+                        <div key={property.id}>
+                            <Listing property={property} />
+                        </div>
+                    )
+                }):null}
             </div>
         </Layout>
     )
---
+++ app/javascript/src/hosting/listing.jsx
@@ -0,0 +1,88 @@
+import { map } from 'jquery';
+import React, {useState, useEffect} from 'react';
+import { handleErrors } from '@utils/fetchHelper';
+
+export default function Listing (props) {
+    const [displayBookings, setDisplayBookings] = useState(false)
+    const [bookings, setBookings] = useState([])
+    const [property, setProperty] = useState(null)
+
+    useEffect( ()=> {
+        fetch(`api/properties/${props.property.id}/bookings`)
+        .then(handleErrors)
+        .then(data => {
+            console.log(data)
+            setBookings(data.bookings)
+        })
+    }, [])
+    
+    useEffect( ()=>{
+        setProperty(props.property)
+    }, [] )
+
+    const clickHandler = () => {
+        setDisplayBookings(!displayBookings)
+    }
+
+    let placeholderPic = "https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=6&m=1040250650&s=612x612&w=0&h=Ve0znmMwCbVyo66uIfeSrSYRuHau85oBiVIv1OplATs="
+
+    const noBookings = () => {
+        return (
+            <div className="py-3">
+                <h5>No Bookings found for this property</h5>
+            </div>
+        )
+    }
+
+    if (property) {
+
+        return (
+            <>
+            <div className="row my-3 pt-2">
+                <div className="col-4 listing-preview pr-sm-1 pr-md-0">
+                    <div key={props.key} className="card" style={{width: "14rem"}}>
+                        <img className="card-img-top" src={property.image_url? property.image_url:placeholderPic} alt="Property picture" />
+                        <div className="card-body text-center">
+                            <button className="card-title btn btn-secondary btn-lg btn-block" onClick={clickHandler}>Upcoming Bookings</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-5 my-3">
+                    <h5>{property.title}</h5>
+                    <p>{property.description}</p>
+                    {/* <p>{property.city}</p>
+                    <p>{property.price_per_night}</p> */}
+                </div>
+                <div className="col-3 d-flex flex-column my-3">
+                    {/* TODO add listing page redirect with listing id once the listing page has been created */}
+                    <a href="" role="button" className="btn btn-danger my-2">Edit listing</a>
+                    <button className="btn btn-danger my-2">Disable listing</button>
+                    <button className="btn btn-danger my-2">Delete listing</button>
+                </div>
+                <div className={displayBookings? "pb-2 pt-3":"pb-2 pt-3 d-none"}>
+                    {bookings.length > 0? <h5 className="pb-2">Upcoming bookings:</h5>: <h5 className="pb-2">No bookings found for this property</h5>}
+                    {bookings.map( booking => {
+
+                        return (
+                            <div key={booking.id} className="row">
+                                <div className="col-4">
+                                    <div className="card" style={{width: "11rem"}}>
+                                        <ul className="list-group list-group-flush">
+                                            <li className="list-group-item">Guest: {booking.guest}</li>
+                                            <li className="list-group-item">Start: {booking.start_date}</li>
+                                            <li className="list-group-item">End: {booking.end_date}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        )  
+                    })}
+                </div>
+            </div>
+            <hr />
+            </>
+        )
+    } else {
+        return null
+    }
+} 
